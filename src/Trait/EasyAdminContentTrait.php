1e9c13f727aeaa42732c4828425b21d004cae21f
aleksey <example@example.com>
2026-02-26 13:11:27 +0400

    refactor(src/Trait/EasyAdminContentTrait.php): Update `renderContent` method to accept either an array of ContentInterface or a single ContentInterface.

--- src/Trait/EasyAdminContentTrait.php
+++ src/Trait/EasyAdminContentTrait.php
@@ -4,25 +4,30 @@ namespace Wertelko\EasyadminContentBundle\Trait;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Wertelko\EasyadminContentBundle\Content\Configurator\HeadingContentConfigurator;
 use Wertelko\EasyadminContentBundle\Content\ContentInterface;
 use Wertelko\EasyadminContentBundle\Factory\ContentFactory;
 
 trait EasyAdminContentTrait
 {
     protected ContentFactory $contentFactory;
+
     /**
      * @param array<ContentInterface> $content
+     * @param string $title
      * @return Response
      * @throws \Exception
      */
     public function renderContent(
-        array $content = [],
-        string $title = '',
+        array|ContentInterface $content = [],
+        string                 $title = '',
     ): Response
     {
         if (!($this instanceof AbstractController)) {
             throw new \Exception(sprintf("Controller must implement %s", AbstractController::class));
+        }
+
+        if ($content instanceof ContentInterface) {
+            $content = [$content];
         }
 
         $contentDto = [];
